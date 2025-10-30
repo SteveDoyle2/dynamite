4a74c7b6d8bbbf82e6ea9348075cf350e293aed5
Steve Doyle <sdoyle@anduril.com>
2025-10-30 13:39:04 -0700

    generalizing

--- dynamight/core/str_dynamics.py
+++ dynamight/core/str_dynamics.py
@@ -3,29 +3,23 @@ import numpy as np
 
 
 def _get_iedges(iedge: int, nedges: int) -> tuple[int, int, int]:
-    # sa = [0, 1, 2, 3]
-    sb = [1, 2, 3, 0]
-    sc = [2, 3, 0, 1]
-    sd = [3, 0, 1, 2]
-    i2 = sb[iedge]
-    i3 = sc[iedge]
-    i4 = sd[iedge]
-    return i2, i3, i4
-
-
-def _get_iedges2(iedge: int, nedges: int) -> tuple[int, int, int]:
+    """
+    get ith value of looping list with repeating
+    so:
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]  # iedge=0
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]  # iedge=1
+    [2, 3, 4, 5, 6, 7, 8, 9, 0, 1]  # iedge=2
+    [3, 4, 5, 6, 7, 8, 9, 0, 1, 2]  # iedge=4
+    ...
+    because that's a pain, we basically do this...
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,]
+    """
     values = np.arange(nedges)
-    #i1 = values[(iedge) % nedges]
     i2 = values[(iedge+1) % nedges]
     i3 = values[(iedge+2) % nedges]
     i4 = values[(iedge+3) % nedges]
-    # sa = [0, 1, 2, 3]
-    # sb = [1, 2, 3, 0]
-    # sc = [2, 3, 0, 1]
-    # sd = [3, 0, 1, 2]
-    # i2 = sb[iedge]
-    # i3 = sc[iedge]
-    # i4 = sd[iedge]
     return i2, i3, i4
 
 
@@ -33,36 +27,19 @@ def reduce_line_nsm(points,
                     thickness: float,
                     total_mass: float,
                     ax: plt.Axes,
-                    num_interp: int=10,
+                    num_interp: int=360,
                     nround: int=6) -> tuple[np.ndarray, np.ndarray]:
     """Handles general shapes. Use more points for curved shapes."""
     ts = np.linspace(0., 1., num=num_interp, endpoint=False)
-    # l1 = [p1, p2]
-    # l2 = [p2, p3]
-    # l3 = [p3, p4]
-    # l4 = [p4, p1]
-    # cg = (p1 + p2 + p3 + p4) / 4.
-    # edges = [l1, l2, l3, l4]
     k = np.array([0., 0., 1.])
-
-    # j = np.zeros((4, 3))
-    # for iedge, edge in enumerate(edges):
-    #     pa, pb = edge
-    #     i = pb - pa
-    #     j[iedge, :] = np.cross(k, i)
-    # j2
 
     nedges = len(points)
     points2 = points.copy()
     for iedge, p1 in enumerate(points):
-        i2, i3, i4 = _get_iedges2(iedge, nedges)
-        i2b, i3b, i4b = _get_iedges2(iedge, nedges)
-        assert i2 == i2b
-        assert i3 == i3b
-        assert i4 == i4b
+        i2, i3, i4 = _get_iedges(iedge, nedges)
         p2 = points[i2]
         p3 = points[i3]
-        p4 = points[i4]
+        #p4 = points[i4]
 
         i21 = p2 - p1
         i32 = p3 - p2
@@ -168,7 +145,7 @@ def line_line_intersection(p1, p2, p3, p4):
 def reduce_area_nsm(points: np.ndarray,
                     mass: float,
                     ax: plt.Axes,
-                    num_interp: int=10,
+                    num_interp: int=40,
                     nround: int=6) -> tuple[np.ndarray, np.ndarray]:
     """TODO: handle general shapes"""
     dab = 2.0 / num_interp
@@ -215,6 +192,8 @@ def reduce_area_nsm(points: np.ndarray,
         N31[:, np.newaxis] * p3 +
         N41[:, np.newaxis] * p4)
     pa2 = N1 @ points
+    assert np.allclose(pa, pa2)
+
     # print('N11', N11)
     # print('N21', N21)
     # print('N31', N31)
@@ -231,6 +210,7 @@ def reduce_area_nsm(points: np.ndarray,
         N42[:, np.newaxis] * p4
     )
     pb2 = N2 @ points
+    assert np.allclose(pb, pb2)
     # print('N12', N12)
     # print('N22', N22)
     # print('N32', N32)
@@ -245,6 +225,7 @@ def reduce_area_nsm(points: np.ndarray,
           N33[:, np.newaxis] * p3 +
           N43[:, np.newaxis] * p4)
     pc2 = N3 @ points
+    assert np.allclose(pc, pc2)
 
     # shape functions for p4
     N4, N14, N24, N34, N44 = get_nquad(ab4)
@@ -255,6 +236,7 @@ def reduce_area_nsm(points: np.ndarray,
           N34[:, np.newaxis] * p3 +
           N44[:, np.newaxis] * p4)
     pd2 = N4 @ points
+    assert np.allclose(pd, pd2)
 
     # ax.plot(pd[:, 0], pd[:, 1], 'o')
     p31 = pc - pa
@@ -342,9 +324,22 @@ def main():
     ax = fig.gca()
     ax.set_aspect('equal')
     ax.grid(True)
-    is_rectangle = False
-    is_circle = True
-    if is_rectangle:  # rectangle
+    is_rectangle = True
+    is_circle = False
+    is_line = True
+    if is_line:
+        points = np.array([
+            [0., 0., 0.],
+            [0., 2., 0.],
+            [0.001, 2., 0.],
+            [0.001, 0., 0.],
+        ])
+        thickness = 0.1
+        length = 2.0
+        Ix = 1/12 * mass * length**2
+        Iy = 1/12 * mass * length**2
+        Iz = 0. #1/3 * mass * length**2
+    elif is_rectangle:  # rectangle
         points = np.array([
             [0., 0., 0.],
             [0., 2., 0.],
@@ -360,6 +355,7 @@ def main():
         Iy = 1/12 * mass * a**2
         Iz = 1/12 * mass * (a**2 + b**2)
     else:
+        assert is_circle, f'is_circle {is_circle}'
         npoints = 360
         radius = 2.14
         Ix = mass * radius**2 / 2
@@ -392,6 +388,7 @@ def main():
     ax.plot(points_closed[:, 0], points_closed[:, 1], '-', label='raw')
 
     cg_total, inertia = reduce_line_nsm(points, thickness, mass, ax, num_interp=40)
+    #cg_total, inertia = reduce_line_nsm(points, thickness, mass, ax, num_interp=40)
     #cg_total, inertia = reduce_area_nsm(points, mass, ax, num_interp=40)
     ixx, iyy, izz, ixy, ixz, iyz = inertia
     cgx, cgy, cgz = cg_total
