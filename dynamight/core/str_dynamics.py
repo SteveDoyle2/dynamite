b1cd373006edd2bc41dbbc3e03713d36a76ec9af
Steve Doyle <sdoyle@anduril.com>
2025-10-31 13:12:54 -0700

    better reduce_n_minus1_sided_thickness

--- dynamight/core/str_dynamics.py
+++ dynamight/core/str_dynamics.py
@@ -1,3 +1,4 @@
+from itertools import count
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -23,23 +24,103 @@ def _get_iedges(iedge: int, nedges: int) -> tuple[int, int, int]:
     return i2, i3, i4
 
 
-def reduce_line_nsm(points,
-                    thickness: float,
-                    total_mass: float,
-                    ax: plt.Axes,
-                    num_interp: int=360,
-                    nround: int=6) -> tuple[np.ndarray, np.ndarray]:
-    """Handles general shapes. Use more points for curved shapes."""
-    ts = np.linspace(0., 1., num=num_interp, endpoint=False)
-    k = np.array([0., 0., 1.])
+def reduce_n_minus1_sided_thickness(
+        points: np.ndarray,
+        thickness: float,
+        total_mass: float) -> tuple[np.ndarray, np.ndarray, list[np.ndarray],
+                                    np.ndarray, np.ndarray]:
+    npoints = len(points)
+    nedges = npoints - 1
+    points2, points3 = _get_internal_line_points(points, -thickness)
+    assert nedges == 3
 
+    # reorg points2 to match the order of points
+    i0 = npoints - 1   # TODO: is this always right?
+    ii = np.arange(npoints)
+    ii2 = np.hstack([ii, ii])
+    ii2 = ii2[i0:i0+npoints]
+    points2 = points2[ii2]
+    del points3
+
+    normal = np.array([1., 0., 0.])
+    points_external = points.copy()
+    points_internal = points2.copy()
+    points_internal[0, :] = points[0, :] + normal*thickness
+    points_internal[-1, :] = points[-1, :] - normal*thickness
+
+    # get the points/area by section
+    points_quads = []
+    area = np.zeros(nedges)
+    for iedge in range(nedges):
+        p1 = points_external[iedge, :]
+        p2 = points_external[iedge+1, :]
+        p1_prime = points_internal[iedge, :]
+        p2_prime = points_internal[iedge+1, :]
+        pointsi = np.vstack([p1, p1_prime, p2_prime, p2])
+        a = p1 - p2_prime
+        b = p2 - p1_prime
+        areai = 0.5 * np.linalg.norm(np.cross(a, b))
+        points_quads.append(pointsi)
+        area[iedge] = areai
+
+    total_area = area.sum()
+    narea = len(area)
+    scale = area / total_area
+    mass = scale * total_mass
+
+    # find the total cg of all sections
+    cg = np.zeros((narea, 3))
+    inertia = np.zeros((narea, 6))
+    for iarea, pointsi, areai, massi in zip(count(), points_quads, area, mass):
+        cgi, inertiai = reduce_area_nsm(
+            pointsi, massi,
+            ax=None, num_interp=10, nround=6, add_data_to_plots=False)
+        cg[iarea, :] = cgi
+        inertia[iarea, :] = inertiai
+
+    cg_total, inertia_total = combine_area_based_mass_cg_inertia(
+        area, mass, cg, inertia)
+    return points_internal, points_external, points_quads, cg_total, inertia_total
+
+
+def combine_area_based_mass_cg_inertia(
+        area: np.ndarray,
+        mass: np.ndarray,
+        cg: np.ndarray,
+        inertia: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    total_mass = mass.sum()
+    #cg_mass = (cg * mass[:, np.newaxis]) / total_mass
+    cg_total = (cg * mass[:, np.newaxis]).sum(axis=0) / total_mass
+
+    dxyz = cg - cg_total[np.newaxis, :]
+    dx = dxyz[:, 0]
+    dy = dxyz[:, 1]
+    dz = dxyz[:, 2]
+    ixx = (inertia[:, 0] + area * (dy**2 + dz**2)).sum()
+    iyy = (inertia[:, 1] + area * (dx**2 + dz**2)).sum()
+    izz = (inertia[:, 2] + area * (dx**2 + dy**2)).sum()
+    ixy = (inertia[:, 3] + area * (dx*dy)).sum()
+    ixz = (inertia[:, 4] + area * (dx*dz)).sum()
+    iyz = (inertia[:, 5] + area * (dy*dz)).sum()
+
+    inertia_total = np.array([ixx, iyy, izz, ixy, ixz, iyz])
+    # print('cgs\n', cg)
+    # print('cg_mass\n', cg_mass)
+    # print('cg-total', cg_total)
+    # print('dxyz', dxyz)
+    # inertia = get_inertia(dmass, xyz, cg_total, nround=nround)
+    return cg_total, inertia_total
+
+
+def _get_internal_line_points(points: np.ndarray,
+                              thickness: float) -> tuple[np.ndarray, np.ndarray]:
+    k = np.array([0., 0., 1.])
     nedges = len(points)
     points2 = points.copy()
     for iedge, p1 in enumerate(points):
         i2, i3, i4 = _get_iedges(iedge, nedges)
         p2 = points[i2]
         p3 = points[i3]
-        #p4 = points[i4]
 
         i21 = p2 - p1
         i32 = p3 - p2
@@ -57,15 +138,30 @@ def reduce_line_nsm(points,
             p1_prime1, p2_prime1,
             p2_prime2, p3_prime2)
         points2[iedge, :] = pint2
-        # ax.scatter(pint2[0], pint2[1], )
     # assert len(points2) == 4
     points3 = np.vstack([points2, points2[0, :]])
+    return points2, points3
+
+
+def reduce_line_nsm(points,
+                    thickness: float,
+                    total_mass: float,
+                    ax: plt.Axes,
+                    num_interp: int=360,
+                    nround: int=6) -> tuple[np.ndarray, np.ndarray]:
+    """
+    Handles general shapes. Use more points for curved shapes.
+
+    TODO: Can't do a 1D rod, do that with an area.
+    """
+    ts = np.linspace(0., 1., num=num_interp, endpoint=False)
+    nedges = len(points)
+    points2, points3 = _get_internal_line_points(points, thickness)
     ax.plot(points[:, 0], points[:, 1], marker='s', linestyle='-', label='outer', color='C1')
     ax.plot(points3[:, 0], points3[:, 1], '--x', color='C0')
 
     xyz_list = []
     lengths = []
-    # nedges = 4
     nx = len(ts)
     short_length = 0.
     for iedge in range(nedges):
@@ -89,12 +185,14 @@ def reduce_line_nsm(points,
     assert len(xyz) == len(length), (len(xyz), len(length))
 
     cg1 = np.mean(points, axis=0)
-    cg2 = np.mean(points2, axis=0)
-    # cg3 = np.mean(points3, axis=0)
+    cg2 = np.mean(points2, axis=0)  # internal points
+    # cg3 = np.mean(points3, axis=0) # don't use this; tacks on another point to close the quad
     #dmass = total_mass * length / total_length
     dmass = total_mass * length / short_length
     # assert dmass.sum() == total_mass, (dmass.sum(), total_mass)
     cg_total = (xyz * dmass[:, np.newaxis]).sum(axis=0) / total_mass
+    cg_total = cg_total.round(nround)
+
     print(f"cg_total = {cg_total}")
     ax.scatter(cg_total[0], cg_total[1], marker='o', label='cg', color='C2')
     ax.scatter(cg2[0], cg2[1], marker='o', label='cg-1/ax2', color='C1')
@@ -102,7 +200,6 @@ def reduce_line_nsm(points,
     # ax.scatter(cg3[0], cg3[1], marker='o', label='cg-3')
     inertia = get_inertia(dmass, xyz, cg_total, nround=nround)
     ixx, iyy, izz, ixy, ixz, iyz = inertia
-    # break
 
     fig = ax.get_figure()
     fig.suptitle(f'line smear; mass={total_mass:g} cg=[{cg_total[0]:g}, {cg_total[1]:g}, {cg_total[2]:g}]\n'
@@ -146,32 +243,27 @@ def reduce_area_nsm(points: np.ndarray,
                     mass: float,
                     ax: plt.Axes,
                     num_interp: int=40,
-                    nround: int=6) -> tuple[np.ndarray, np.ndarray]:
+                    nround: int=6,
+                    add_data_to_plots: bool=True) -> tuple[np.ndarray, np.ndarray]:
     """TODO: handle general shapes"""
     dab = 2.0 / num_interp
     t0 = -1 + dab/2
     t1 = 1 - dab/2
-    print(t0, t1, dab)
+    #print(t0, t1, dab)
 
     p1 = points[0, :][np.newaxis, :]
     p2 = points[1, :][np.newaxis, :]
     p3 = points[2, :][np.newaxis, :]
     p4 = points[3, :][np.newaxis, :]
-    # a, b = np.meshgrid(np.linspace(-.95, 0.95, num=19, endpoint=True),
-    #                    np.linspace(-.95, 0.95, num=19, endpoint=True))
     xs = np.linspace(t0, t1, num=num_interp, endpoint=True)
 
     a, b = np.meshgrid(xs, xs)
-    # a, b = np.meshgrid(np.linspace(-.95, 0.95, num=2, endpoint=True),
-    #                    np.linspace(-.95, 0.95, num=2, endpoint=True))
     a = a.flatten()
     b = b.flatten()
-
     ab = np.column_stack([a, b])
 
     npoints = len(ab)
-    assert npoints > 5, npoints
-    # print(f'npoints = {npoints}')
+    # assert npoints > 5, npoints
     ab1 = np.column_stack([ab[:, 0]-dab/2, ab[:, 1]-dab/2])
     ab2 = np.column_stack([ab[:, 0]+dab/2, ab[:, 1]-dab/2])
     ab3 = np.column_stack([ab[:, 0]+dab/2, ab[:, 1]+dab/2])
@@ -184,64 +276,25 @@ def reduce_area_nsm(points: np.ndarray,
 
     # shape functions for p1
     # abss = np.column_stack([ab1, ab2, ab3, ab4])
-    N1, N11, N21, N31, N41 = get_nquad(ab1)
+    N1 = get_nquad(ab1)
+    pa = N1 @ points
 
-    pa = (
-        N11[:, np.newaxis] * p1 +
-        N21[:, np.newaxis] * p2 +
-        N31[:, np.newaxis] * p3 +
-        N41[:, np.newaxis] * p4)
-    pa2 = N1 @ points
-    assert np.allclose(pa, pa2)
-
-    # print('N11', N11)
-    # print('N21', N21)
-    # print('N31', N31)
-    # print('N41', N41)
-    # print('pa', pa)
-
-    # print('ab2', ab2)
     # shape functions for p2
-    N2, N12, N22, N32, N42 = get_nquad(ab2)
-    pb = (
-        N12[:, np.newaxis] * p1 +
-        N22[:, np.newaxis] * p2 +
-        N32[:, np.newaxis] * p3 +
-        N42[:, np.newaxis] * p4
-    )
-    pb2 = N2 @ points
-    assert np.allclose(pb, pb2)
-    # print('N12', N12)
-    # print('N22', N22)
-    # print('N32', N32)
-    # print('N42', N42)
-    # print('pb', pb)
+    N2 = get_nquad(ab2)
+    pb = N2 @ points
 
     # shape functions for p3
-    N3, N13, N23, N33, N43 = get_nquad(ab3)
-
-    pc = (N13[:, np.newaxis] * p1 +
-          N23[:, np.newaxis] * p2 +
-          N33[:, np.newaxis] * p3 +
-          N43[:, np.newaxis] * p4)
-    pc2 = N3 @ points
-    assert np.allclose(pc, pc2)
+    N3 = get_nquad(ab3)
+    pc = N3 @ points
 
     # shape functions for p4
-    N4, N14, N24, N34, N44 = get_nquad(ab4)
-    assert len(N24) == npoints
-    #print('N2:',N24)
-    pd = (N14[:, np.newaxis] * p1 +
-          N24[:, np.newaxis] * p2 +
-          N34[:, np.newaxis] * p3 +
-          N44[:, np.newaxis] * p4)
-    pd2 = N4 @ points
-    assert np.allclose(pd, pd2)
+    N4 = get_nquad(ab4)
+    assert len(N2) == npoints
+    pd = N4 @ points
 
     # ax.plot(pd[:, 0], pd[:, 1], 'o')
     p31 = pc - pa
     p42 = pd - pb
-    #print(p31.shape, p42.shape)
     axb = np.cross(p31, p42, axis=1)
     assert axb.shape == p31.shape, (axb.shape, p31.shape)
     area = 0.5 * np.linalg.norm(axb, axis=1)
@@ -267,16 +320,17 @@ def reduce_area_nsm(points: np.ndarray,
     dmass = mass * area / area_total
 
     cg_total = (p1 + p2 + p3 + p4) / 4
-    ax.plot(cg_total[:, 0], cg_total[:, 1], 'o', label='cg')
     inertia = get_inertia(dmass, xyz, cg_total, nround=nround)
     ixx, iyy, izz, ixy, ixz, iyz = inertia
 
-    fig = ax.get_figure()
     cg_total = cg_total.flatten()
-    fig.suptitle(f'area smear; mass={mass:g} cg=[{cg_total[0]:g}, {cg_total[1]:g}, {cg_total[2]:g}]\n'
-                 f'inertia=[{ixx:g}, {ixy:g}, {iyy:g}, {ixz:g}, {iyz:g}, {izz:g}]')
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
+    if add_data_to_plots:
+        fig = ax.get_figure()
+        ax.plot(cg_total[:, 0], cg_total[:, 1], 'o', label='cg')
+        fig.suptitle(f'area smear; mass={mass:g} cg=[{cg_total[0]:g}, {cg_total[1]:g}, {cg_total[2]:g}]\n'
+                     f'inertia=[{ixx:g}, {ixy:g}, {iyy:g}, {ixz:g}, {iyz:g}, {izz:g}]')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
     return cg_total, inertia
 
 
@@ -309,36 +363,54 @@ def get_inertia(dmass: np.ndarray,
     return inertia
 
 
-def get_nquad(ab):
-    N12 = (1 - ab[:, 0]) * (1 - ab[:, 1]) / 4
-    N22 = (1 + ab[:, 0]) * (1 - ab[:, 1]) / 4
-    N32 = (1 + ab[:, 0]) * (1 + ab[:, 1]) / 4
-    N42 = (1 - ab[:, 0]) * (1 + ab[:, 1]) / 4
-    N2 = np.column_stack([N12, N22, N32, N42])
-    return N2, N12, N22, N32, N42
+def get_nquad(ab: np.ndarray) -> np.ndarray:
+    N1 = (1 - ab[:, 0]) * (1 - ab[:, 1]) / 4
+    N2 = (1 + ab[:, 0]) * (1 - ab[:, 1]) / 4
+    N3 = (1 + ab[:, 0]) * (1 + ab[:, 1]) / 4
+    N4 = (1 - ab[:, 0]) * (1 + ab[:, 1]) / 4
+    Ns = np.column_stack([N1, N2, N3, N4])
+    return Ns
 
 
-def main():
+def main() -> None:
     mass = 2
     fig = plt.figure()
     ax = fig.gca()
     ax.set_aspect('equal')
     ax.grid(True)
-    is_rectangle = True
+    is_rectangle = False
     is_circle = False
-    is_line = True
+    is_line = False
+    is_rectangle3 = True
+    close_points = True
     if is_line:
         points = np.array([
             [0., 0., 0.],
             [0., 2., 0.],
-            [0.001, 2., 0.],
-            [0.001, 0., 0.],
+            [0.1, 2., 0.],
+            [0.1, 0., 0.],
         ])
-        thickness = 0.1
+        #thickness = 0.1
         length = 2.0
         Ix = 1/12 * mass * length**2
-        Iy = 1/12 * mass * length**2
-        Iz = 0. #1/3 * mass * length**2
+        Iy = 0.
+        Iz = 1/12 * mass * length**2
+    elif is_rectangle3:
+        thickness = 0.2
+        close_points = False
+        points = np.array([
+            [0., 0., 0.],
+            [0., 1.25, 0.],
+            [1.2, 1.25, 0.],
+            [1.2, 0., 0.],
+        ])
+        a = 1.
+        b = 2.
+        # wrong
+        Ix = 1/12 * mass * b**2
+        Iy = 1/12 * mass * a**2
+        Iz = 1/12 * mass * (a**2 + b**2)
+
     elif is_rectangle:  # rectangle
         points = np.array([
             [0., 0., 0.],
@@ -384,12 +456,25 @@ def main():
     #     x = [pa[0], pb[0]]
     #     y = [pa[1], pb[1]]
     #     ax.plot(x, y, '-')
-    points_closed = np.vstack([points, points[0, :],])
-    ax.plot(points_closed[:, 0], points_closed[:, 1], '-', label='raw')
+    if close_points:
+        points_closed = np.vstack([points, points[0, :],])
+        ax.plot(points_closed[:, 0], points_closed[:, 1], '-', label='raw')
+    else:
+        ax.plot(points[:, 0], points[:, 1], '-', label='raw')
 
-    cg_total, inertia = reduce_line_nsm(points, thickness, mass, ax, num_interp=40)
+    if is_circle:
+        aaa
+        cg_total, inertia = reduce_line_nsm(points, thickness, mass, ax, num_interp=40)
+    elif is_line or is_rectangle:
+        bbb
+        cg_total, inertia = reduce_area_nsm(points, mass, ax, num_interp=40)
+    elif is_rectangle3:
+        cg_total, inertia = reduce_n_minus1_sided_thickness(
+            points, thickness, mass,
+            ax)
+    else:
+        raise RuntimeError((is_circle, is_line, is_rectangle))
     #cg_total, inertia = reduce_line_nsm(points, thickness, mass, ax, num_interp=40)
-    #cg_total, inertia = reduce_area_nsm(points, mass, ax, num_interp=40)
     ixx, iyy, izz, ixy, ixz, iyz = inertia
     cgx, cgy, cgz = cg_total
 
